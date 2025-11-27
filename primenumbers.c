48d5598ed679e18a35390475e675ec7b752965f5
Shanoar Showrov <shanoarshowrov@gmail.com>
2025-11-27 16:44:18 +0600

    Check a number prime or not prime

---
+++ primenumbers.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main ()
+{
+ int i,n,count=0;
+ printf("## Check a Number is Prime or Not Prime ##\n");
+ printf("Enter a Number=");
+ scanf("%d",&n);
+ for (i=1;i<=n;i++){
+
+    if(n%i==0)
+    {
+        count++;
+
+    }
+ }
+ if(count/2==1)
+ {
+    printf("%d is a Prime Number");
+ }
+ else
+ {
+    printf("%d is Not a Prime Number");
+ }
+
+
+
+
+    return 0;
+
+}
+primenumbers.c
